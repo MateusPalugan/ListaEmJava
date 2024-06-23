ed94bb20486f1d5bedc0a757c78441db4bdf16da
MateusPalugan <mateus.palugan@digitalsys.tech>
2024-06-23 19:19:00 -0300

    update code

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
